e1b3ac5ba4192ab80bc296fe05951d5ccf9ff0b4
Jokimax <77680901+Jokimax@users.noreply.github.com>
2022-11-18 18:55:22 +0200

    Update pivot_sort.py

--- pivot_sort.py
+++ pivot_sort.py
@@ -1,27 +1,31 @@
-def pivot_sort(arr, min_Value = 0, max_Value = 2^64):
+def pivot_sort(arr, min_Value = 0, max_Value = 2**64):
+    # Set up pivot and array
     avg = sum(arr)/len(arr)
     new_arr = [max_Value] * (len(arr) + 1)
     i = 0
     x = 0
+    # Create new_arr values
     while i < len(arr):
         if avg > arr[i]:
             x += 1
         i += 1
     new_arr[x] = avg
-    minus = x
-    plus = x
     i = 0
     while i < x:
         new_arr[i] = min_Value
         i += 1
+    minus = x
+    plus = x
     i = 0
+    # Loop through the array
     while i < len(arr):
         n = x
-        if arr[i] < avg:
+        if arr[i] < avg: # Check if current value is lower than the pivot
             n -= 1
             minus -= 1
             while n >= minus:
-                if arr[i] > new_arr[n]:
+                if arr[i] > new_arr[n]: # Check if current value is higher than the set value in new_arr
+                    # If true push all values in new_arr
                     temp = new_arr[n]
                     new_arr[n] = arr[i]
                     while n > minus:
@@ -34,7 +38,8 @@ def pivot_sort(arr, min_Value = 0, max_Value = 2^64):
             n += 1
             plus += 1 
             while n <= plus:
-                if arr[i] < new_arr[n]:
+                if arr[i] < new_arr[n]: # Check if current value is lower than the set value in new_arr
+                    # If true push all values in new_arr
                     temp = new_arr[n]
                     new_arr[n] = arr[i]
                     while n < plus:
